36e89ebb65e6547f3ae544177037d914d3fb731d
gjh <guojinhui4@gmail.com>
2021-01-10 20:06:21 +0800

    fix small typo

--- scripts/rosbag/sync-data.py
+++ scripts/rosbag/sync-data.py
@@ -16,38 +16,39 @@ import sys
 import pandas as pd
 import numpy as np
 
-# Data folder argument
-folder= sys.argv[1]
-for i in range(1,24):  
-    #Read data
-    path= folder+'/advio-'+str(i).zfill(2)+'/iphone/arkit.csv'
-    arkit=pd.read_csv(path,names=list('tabcdefg'))
-    path= folder+'/advio-'+str(i).zfill(2)+'/iphone/accelerometer.csv'
-    acc=( pd.read_csv(path,names=list('tabc')))
-    path= folder+'/advio-'+str(i).zfill(2)+'/iphone/gyro.csv'
-    gyro=( pd.read_csv(path,names=list('tabc')))
-    g=[]
-    a=[]
-    t=np.array(list(map(float,gyro[list('t')].values)))
-    zer=t*0.0
-    # Make imu
+folder = sys.argv[1]
+
+for i in range(1, 24):
+    path = folder + '/advio-' + str(i).zfill(2) + '/iphone/arkit.csv'
+    arkit = pd.read_csv(path, names=list('tabcdefg'))
+    path = folder + '/advio-' + str(i).zfill(2) + '/iphone/accelerometer.csv'
+    acc = (pd.read_csv(path, names=list('tabc')))
+    path = folder + '/advio-' + str(i).zfill(2) + '/iphone/gyro.csv'
+    gyro = (pd.read_csv(path, names=list('tabc')))
+
+    # make imu data
+    g = []
+    a = []
+    t = np.array(list(map(float, gyro[list('t')].values)))
+    zero = t * 0.0
     for c in 'abc':
-    # Interpolate accelerometer data to match gyroscope timestamps.
-        a.append(np.interp( np.array(list(map(float,gyro[list('t')].values))), np.array(list(map(float,acc[list('t')].values))), np.array(list(map(float,acc[list(c)].values)))))
-        g.append(np.array(list(map(float,gyro[list(c)].values))))
-    M=np.column_stack((t,zer+34,g[0],g[1],g[2],a[0],a[1],a[2],zer,zer))
-    v=[]
-    t=np.array(list(map(float,arkit[list('t')].values)))
-    t=t-t[0]
+        # interpolate accelerometer data to match gyroscope timestamps
+        a.append(np.interp(np.array(list(map(float, gyro[list('t')].values))), np.array(list(map(float, acc[list('t')].values))), np.array(list(map(float, acc[list(c)].values)))))
+        g.append(np.array(list(map(float, gyro[list(c)].values))))
+    M = np.column_stack((t, zero + 34, g[0], g[1], g[2], a[0], a[1], a[2], zero, zero))
 
-
-    zer=t*0
-    #Make arkit data
+    # make arkit data
+    v = []
+    t = np.array(list(map(float, arkit[list('t')].values)))
+    t = t - t[0] # note this
+    zero = t * 0
     for c in 'abcdefg':
-        v.append(np.array(list(map(float,arkit[list(c)].values))))
-    Mkit=np.column_stack((t,zer+7,np.arange(len(zer)),v[0],v[1],v[2],v[3],v[4],v[5],v[6]))
-    full=np.concatenate((M,Mkit))
-    #sort to time vector
-    full = full[full[:,0].argsort()]
-    path= folder+'/advio-'+str(i).zfill(2)+'/iphone/imu-gyro.csv'
-    np.savetxt(path, full, delimiter=",",fmt='%.6f')
+        v.append(np.array(list(map(float, arkit[list(c)].values))))
+    Mkit = np.column_stack((t, zero + 7, np.arange(len(zero)), v[0], v[1], v[2], v[3], v[4], v[5], v[6]))
+
+    # combine imu and arkit, then sort to time vector
+    full = np.concatenate((M, Mkit))
+    full = full[full[:, 0].argsort()]
+
+    path = folder + '/advio-' + str(i).zfill(2) + '/iphone/imu-gyro.csv'
+    np.savetxt(path, full, delimiter=",", fmt='%.6f')
